e3b3b8ec272c209b5db254a0c21d644da8fcca46
Alexander Karpov <alexander.karpov@neosperience.com>
2017-08-30 12:15:57 +0200

    Lesson 70

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,17 @@
+const User = require('../models/user');
+
 exports.signup = function(req, res, next) {
-  res.send({ success: 'true'});
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // See if a user with the give email exists
+
+  User.findOne({email: email})
+
+  // If a user with email does exist, return an Error
+
+  // if a user with email does NOT exist, create and save user record
+
+  // Repond to reques indicating the user was created
 }
